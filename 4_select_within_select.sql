6879c96edf321601bb05538678638bbb77f6d3c7
jerônimojr <franciscojeronimo@alu.ufc.br>
2021-09-17 11:04:57 -0300

    go

---
+++ 4_select_within_select.sql
@@ -0,0 +1,32 @@
+
+/* 1.List each country name where the population is larger than that of 'Russia'. */
+
+SELECT name FROM world
+ WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
+
+/* 2. Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.*/
+
+SELECT name 
+FROM world
+WHERE continent = 'Europe' AND (gdp/population) > (SELECT 
+                             (gdp/population)
+                            FROM world
+                            WHERE name = 'United Kingdom')
+
+/* 3. List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.*/
+
+SELECT name, continent
+FROM world
+WHERE continent IN (SELECT continent
+       		    FROM world
+                     WHERE name IN ('Argentina', 'Australia'))
+ORDER BY name
+
+
+/* 4. Which country has a population that is more than Canada but less than Poland? Show the name and the population.*/
+
+SELECT name, population
+FROM world
+WHERE population BETWEEN (SELECT population FROM world WHERE name = 'Canada') AND (SELECT population FROM world WHERE name = 'Poland') AND name NOT IN ('Canada', 'Poland')
