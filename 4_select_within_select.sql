761ef08c7eb75bd15a18b6f13c8f0d7c40a936ef
jerônimojr <franciscojeronimo@alu.ufc.br>
2021-09-27 12:52:46 -0300

    ok

--- 4_select_within_select.sql
+++ 4_select_within_select.sql
@@ -30,3 +30,44 @@ ORDER BY name
 SELECT name, population
 FROM world
 WHERE population BETWEEN (SELECT population FROM world WHERE name = 'Canada') AND (SELECT population FROM world WHERE name = 'Poland') AND name NOT IN ('Canada', 'Poland')
+
+/* 5. Germany (population 80 million) has the largest population of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany.
+ Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany. */
+
+SELECT name, CONCAT(ROUND(population /(SELECT population FROM world WHERE name='Germany')*100,0),'%')
+FROM world
+WHERE continent = 'Europe'
+
+
+/* 6. Which countries have a GDP greater than every country in Europe? [Give the name only.] 
+(Some countries may have NULL gdp values) */
+
+SELECT name
+FROM world
+WHERE gdp > ALL(SELECT gdp FROM world WHERE gdp > 0
+ AND continent = 'Europe')
+
+
+/*7. Find the largest country (by area) in each continent, show the continent, the name and the area:*/
+
+SELECT continent, name, area FROM world y
+WHERE area >= ALL(SELECT area FROM world x WHERE x.continent = y.continent)
+
+/* 8. List each continent and the name of the country that comes first alphabetically.*/
+
+SELECT continent, name
+FROM world x
+WHERE name = (SELECT MIN(name) FROM world y WHERE x.continent = y.continent)
+
+
+/* 9. Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents. 
+Show name, continent and population.*/
+
+
+
+/*10. Some countries have populations more than three times that of any of their neighbours 
+(in the same continent). Give the countries and continents.*/
+
+SELECT name, continent
+FROM world x
+WHERE population > ALL(SELECT (3*population) FROM world y WHERE x.continent = y.continent AND y.population <> x.population)
